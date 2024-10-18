74a1965f20a744daeb670d8af6e0271027fa2e64
asenetcky <asenetcky@gmail.com>
2024-10-18 15:12:51 -0400

    add log_message function

--- R/log_message.R
+++ R/log_message.R
@@ -1,3 +1,14 @@
+#' Send a standardized message to stdout or stderr
+#'
+#' @param message A string message to log
+#' @param level The level of the message (e.g. INFO, WARNING, ERROR)
+#' @param out_or_err Whether to log to stdout or stderr
+#'
+#' @return A message is printed to stdout or stderr
+#' @export
+#'
+#' @examples
+#' log_message("This is an info message", "INFO", "OUT")
 log_message <- function(message, level = "INFO", out_or_err = "OUT") {
 
   # check for stdout or stderr
