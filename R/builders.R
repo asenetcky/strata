7d2b13b781fb4eacd88d85824d87c83bd16623fd
asenetcky <asenetcky@gmail.com>
2024-10-19 19:33:15 -0400

    split out functuion families/ideas

--- R/builders.R
+++ R/builders.R
@@ -92,12 +92,3 @@ check_pipeline <- function(pipeline_path) {
 
 }
 
-survey_strata <- function(project_path) {
-  #force to fs::path
-  project_path <- fs::path(project_path)
-
-
-  # read the .tomls, and report out info on the strata project
-
-}
-
