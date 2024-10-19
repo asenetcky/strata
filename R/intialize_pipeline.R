df464252ad0298e7bb7dd73e9d514a0d96147171
asenetcky <asenetcky@gmail.com>
2024-10-18 20:44:52 -0400

    add outline for new fun

---
+++ R/intialize_pipeline.R
@@ -0,0 +1,17 @@
+initialize_pipeline <- function(path, pipeline_name) {
+  # Create the pipelines directory
+
+  # Create the pipeline subdirectories
+  # so a folder for this name pipeline
+  # a subfolder in that of modules
+
+  # Create the pipeline R script
+  #use the usual pipeline_start?
+
+  # Write the pipeline R script
+  # have it start writing out main, or something like that
+  # parameters for order?
+
+  # I guess later on come up with a way to add/remove pipelines
+  #change order and then the same for modules
+}
