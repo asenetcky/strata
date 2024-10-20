5c73aa90ff7a8f6ead56f3c1a6f3e4e2e38ad402
asenetcky <asenetcky@gmail.com>
2024-10-20 16:57:30 -0400

    add write_toml function

--- R/toml.R
+++ R/toml.R
@@ -1,26 +1,27 @@
-write_toml <- function(path, name, stratum_type, order = NULL, skip_if_fail = NULL) {
+  write_toml <- function(path, name, stratum_type, order = 1, skip_if_fail = FALSE) {
   #force to fs::path
   path <- fs::path(path)
 
   # check stratum type
   checkmate::assert_choice(stratum_type, c("pipeline", "module"))
-  stratum_type <-
-    dplyr::if_else(stratum_type == "pipeline", ".pipeline", ".module")
-  toml_type <-
-    dplyr::if_else(stratum_type == ".pipeline", "pipelines", "modules")
+  stratum_folder <- paste0(stratum_type, "s")
+  toml_type <- paste0(".", stratum_folder, ".toml")
 
   # Create .toml
-  toml_file <- fs::path(path, toml_type ,".toml")
+  toml_file <- fs::path(path, stratum_folder, toml_type)
   fs::file_create(toml_file)
 
   # Write out the .toml file
   writeLines(
     paste0(
       #just keep it basic for now add other stuff later
-      "[", toml_type, "]\n",
-      name, " = { created = ", lubridate::today(), " }\n"
+      "[", stratum_folder, "]\n",
+      name, " = { created = ", lubridate::today(),
+      ", order = ", order,
+      ", skip_if_fail = ", skip_if_fail,
+      " }\n"
 
     ),
-    toml_file
+    invisible(toml_file)
   )
 }
