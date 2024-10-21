5af136c36e5d2c1f04ec800bac6c1fe91ea6b562
asenetcky <asenetcky@gmail.com>
2024-10-21 16:44:09 -0400

    add helper functions for main

--- R/main.R
+++ R/main.R
@@ -17,3 +17,31 @@
 #   tidyr::pivot_longer(everything(),
 #                       names_to = "pipeline",
 #                       values_to = "info")
+
+find_pipelines <- function(path = ".") {
+  where_is_main <- fs::path_abs(path)
+  toml_path <- fs::path(where_is_main, "pipelines/.pipelines.toml")
+
+  toml_path |>
+    build_paths()
+
+}
+
+#' @importFrom rlang .data
+build_paths <- function(toml_path) {
+  toml_path <- fs::path(toml_path)
+  toml <- snapshot_toml(toml_path)
+
+  target_path <- fs::path_dir(toml_path)
+
+  toml |>
+    dplyr::arrange(.data$order) |>
+    dplyr::mutate(
+      paths = fs::path(
+        target_path,
+        .data$name
+      )
+    ) |>
+    dplyr::pull(.data$paths)
+
+}
