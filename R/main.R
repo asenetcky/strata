32c2587d93caa37bc090a8b218fa274fdd102440
asenetcky <asenetcky@gmail.com>
2024-10-21 08:20:11 -0400

    add main prototype

--- R/main.R
+++ R/main.R
@@ -1,14 +1,19 @@
-pipeline_toml <-
-  RcppTOML::parseToml(fs::path("./pipelines/.pipelines.toml"))
+# pipeline_toml <-
+#   RcppTOML::parseToml(fs::path("./pipelines/.pipelines.toml"))
+#
+# module_tomls <-
+#   purrr::map(
+#     names(pipeline_toml$pipelines),
+#     \(pipe) {
+#       RcppTOML::parseToml(
+#         fs::path(
+#           paste0("./pipelines/", pipe, "/modules/.modules.toml")
+#         )
+#       )
+#     }
+#   )
 
-module_tomls <-
-  purrr::map(
-    names(pipeline_toml$pipelines),
-    \(pipe) {
-      RcppTOML::parseToml(
-        fs::path(
-          paste0("./pipelines/", pipe, "/modules/.modules.toml")
-        )
-      )
-    }
-  )
+# dplyr::as_tibble(pipeline_toml$pipelines) |>
+#   tidyr::pivot_longer(everything(),
+#                       names_to = "pipeline",
+#                       values_to = "info")
