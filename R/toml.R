7f6244c8a85a8fdfb4b523c5e903e6281db21aa4
asenetcky <asenetcky@gmail.com>
2024-10-21 13:12:29 -0400

    add toml management functions

--- R/toml.R
+++ R/toml.R
@@ -49,3 +49,146 @@ snapshot_toml <- function(toml_path) {
     dplyr::select(dplyr::any_of(vars))
 }
 
+manage_toml_order <- function(toml_snapshot) {
+  duplicate_orders <-
+    !dplyr::n_distinct(toml_snapshot$order) == base::nrow(toml_snapshot)
+
+  if (duplicate_orders) {
+  toml_name <- paste0(".", unique(toml_snapshot$type), ".toml")
+     duped_orders <-
+      toml_snapshot |>
+      dplyr::count(order) |>
+      dplyr::filter(n > 1) |>
+      dplyr::pull(order)
+
+     without_dupes <-
+       toml_snapshot |>
+       dplyr::filter(!order %in% duped_orders) |>
+       dplyr::arrange(order) |>
+       dplyr::mutate(order = dplyr::row_number())
+
+     max_order <- max(without_dupes$order)
+
+     with_dupes <-
+       toml_snapshot |>
+       dplyr::filter(order %in% duped_orders) |>
+       dplyr::arrange() |># for now till I find sensible logic
+       dplyr::mutate(order = max_order + dplyr::row_number())
+
+     toml_snapshot <-
+       dplyr::bind_rows(without_dupes, with_dupes)
+
+     log_message(
+       paste(
+         "Duplicate orders found in the",
+         toml_name,
+         "file, reordering"
+       ),
+       "WARN")
+  }
+  toml_snapshot
+}
+
+backup_toml <- function(toml_path) {
+ file_root <- fs::path_dir(toml_path)
+  file_name <-
+    fs::path_file(toml_path) |>
+    stringr::str_replace("\\.toml", "\\.bak")
+
+  fs::file_copy(toml_path,  fs::path(file_root, file_name), overwrite = TRUE)
+
+  log_message(
+    paste(
+      "Backed up",
+      toml_path,
+      "to",
+      fs::path(file_root, file_name)
+    ),
+    "INFO"
+  )
+}
+
+
+write_toml_lines <- function(toml_content, toml_path) {
+  toml_path <- fs::path(toml_path)
+  toml_type <- base::unique(toml_content$type)
+
+  #TODO make cleaner
+  names <-
+    toml_content |>
+    dplyr::select(dplyr::any_of("name"))
+  orders <-
+    toml_content |>
+    dplyr::select(dplyr::any_of("order"))
+  skip_if_fails <-
+    toml_content |>
+    dplyr::select(dplyr::any_of("skip_if_fail"))
+  created <-
+    toml_content |>
+    dplyr::select(dplyr::any_of("created"))
+
+  header <- paste0("[", toml_type, "]")
+
+  if (purrr::is_empty(skip_if_fails)) {
+    skip_if_fails_text <-
+      dplyr::tibble(
+        skip_if_fail = rep("", base::nrow(toml_content))
+      )
+  } else {
+    skip_if_fails_text <-
+      dplyr::tibble(
+        skip_if_fail =
+          paste0(", skip_if_fail = ", skip_if_fails$skip_if_fail)
+        )
+  }
+
+  lines <-
+    purrr::pmap(
+      list(names, orders, skip_if_fails_text, created),
+      \(name, order, skip_if_fail, created) {
+        paste0(
+          "\n", name, " = { created = ", created,
+          ", order = ", order,
+          skip_if_fail,
+          " }"
+        )
+      }
+    ) |>
+    purrr::reduce(paste0)
+
+  fs::file_create(toml_path)
+
+  cat(
+    header,
+    file = toml_path,
+    append = TRUE
+  )
+
+  cat(
+    lines,
+    file = toml_path,
+    append = TRUE
+  )
+}
+
+
+rewrite_from_snapshot <- function(toml_snapshot, toml_path) {
+  toml_path <- fs::path(toml_path)
+
+  backup_toml(toml_path)
+  fs::file_delete(toml_path)
+
+  #rewrite toml
+  new_toml <-
+    toml_snapshot |>
+    dplyr::mutate(
+      dplyr::across(
+        dplyr::any_of("skip_if_fail"),
+        stringr::str_to_lower
+      )
+    )
+
+  write_toml_lines(new_toml, toml_path)
+  invisible(toml_path)
+}
+
