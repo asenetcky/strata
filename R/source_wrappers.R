41dc49947c75bb3f863e823fe1caee2b08b9e73f
asenetcky <asenetcky@gmail.com>
2024-10-24 08:58:20 -0400

    add proof of concept run execution plan

--- R/source_wrappers.R
+++ R/source_wrappers.R
@@ -1,8 +1,27 @@
 run_execution_plan <- function(execution_plan) {
   strata_start <- lubridate::now()
 
+  initial_pipeline <- execution_plan[1, ]$pipeline
+  initial_module <- execution_plan[1, ]$module_name
+
+  log_message("Strata started")
+  log_message(paste("Pipeline:", initial_pipeline, "initialized"))
+  log_message(paste("Module:", initial_module, "initialized"))
   for (row in seq_len(nrow(execution_plan))) {
     row_scope <- execution_plan[row, ]
+    row_pipeline <- row_scope$pipeline
+    row_module <- row_scope$module_name
+
+
+    if (row_pipeline != initial_pipeline) {
+      log_message(paste("Pipeline:", row_pipeline, "initialized"))
+      initial_pipeline <- row_pipeline
+    }
+
+    if (row_module != initial_module) {
+      log_message(paste("Module:", row_module, "initialized"))
+      initial_module <- row_module
+    }
 
 
     print(row_scope$path)
