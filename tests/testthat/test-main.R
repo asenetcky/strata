bee22a04e42f9dc45d827c96d95c50960482691c
asenetcky <asenetcky@gmail.com>
2024-10-27 12:29:43 -0400

    add tests for main function

---
+++ tests/testthat/test-main.R
@@ -0,0 +1,46 @@
+test_that("main with no path", {
+  expect_error(main())
+})
+
+test_that("main returns execution plan", {
+  tmp <- fs::dir_create(fs::file_temp())
+  strata::build_stratum(
+    path = tmp,
+    stratum_name = "first_stratum",
+    order = 1
+  )
+  stratum_path <-
+    fs::path(
+      tmp, "strata", "first_stratum"
+    )
+  strata::build_lamina(
+    stratum_path = stratum_path,
+    lamina_name = "first_lamina",
+    order = 1
+  )
+  strata::build_lamina(
+    stratum_path = stratum_path,
+    lamina_name = "second_lamina",
+    order = 2
+  )
+
+  first_lamina_code <- fs::path(stratum_path, "first_lamina", "my_code1.R")
+  second_lamina_code <- fs::path(stratum_path, "second_lamina", "my_code2.R")
+
+  my_code1 <- fs::file_create(first_lamina_code)
+  my_code2 <- fs::file_create(second_lamina_code)
+  cat(file = my_code1, "print('Hello, World!')")
+  cat(file = my_code2, "print('Goodbye, World!')")
+
+  execution_plan <- main(tmp)
+  expect_equal(
+    execution_plan |> dplyr::mutate(path = as.character(path)),
+    tibble::tibble(
+      path = c(first_lamina_code, second_lamina_code),
+      stratum = c("first_stratum", "first_stratum"),
+      script_name = c("my_code1", "my_code2"),
+      lamina_name = c("first_lamina", "second_lamina")
+    )
+  )
+
+})
