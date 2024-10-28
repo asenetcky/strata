45c44de7e9246f42ad6e205cac30d818dc989b14
asenetcky <asenetcky@gmail.com>
2024-10-28 12:58:01 -0400

    move log_message tests to general test-logging

--- tests/testthat/test-log_message.R
+++
@@ -1,8 +0,0 @@
-# Test cases for log_message function
-test_that("log message has no error", {
-  expect_no_error(log_message("hello"))
-})
-
-test_that("log message to stderr", {
-  expect_message(log_message("hello", out_or_err = "ERR"))
-})
