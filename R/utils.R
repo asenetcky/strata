26af211c8bca13670a231bd963014d7e375f32b1
asenetcky <asenetcky@gmail.com>
2024-11-14 16:17:57 -0500

    add survey_strata function code

--- R/utils.R
+++ R/utils.R
@@ -35,3 +35,28 @@ check_stratum <- function(stratum_path) {
   # read the .tomls, do they match up?
   !strata_issue
 }
+
+survey_strata <- function(project_path) {
+  stratum <- lamina <- path <- order <- script <-  created <- NULL
+  skip_if_fail <- execution_order <- script_path <- stratum_name <- NULL
+
+  project_path <- fs::path(project_path)
+
+  plan <-
+    build_execution_plan(project_path) |>
+    dplyr::rename(
+      stratum_name = stratum,
+      lamina_name = lamina,
+      execution_order = order,
+      script_name = script,
+      script_path = path
+    ) |>
+    dplyr::relocate(
+      c(skip_if_fail, created),
+      .after = script_path
+    ) |>
+    dplyr::relocate(
+      execution_order,
+      .before = stratum_name
+    )
+}
