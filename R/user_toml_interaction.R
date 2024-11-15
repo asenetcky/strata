5e6a4689ac0e3ff348f6ac6938caea43d5f51128
asenetcky <asenetcky@gmail.com>
2024-11-15 13:32:42 -0500

    add check_toml_dataframe function
    some checks and guardrails for tomls

--- R/user_toml_interaction.R
+++ R/user_toml_interaction.R
@@ -43,7 +43,45 @@ view_toml <- function(toml_path) {
 #' edit_toml("PATH/TO/ORIGINAL/TOML/FILE.toml", new_toml_dataframe)
 #' }
 edit_toml <- function(original_toml_path, new_toml_dataframe) {
-  #TODO add checks and guardrails
+  new_toml_dataframe <-
+    manage_toml_order(new_toml_dataframe) |>
+    check_toml_dataframe()
+
   rewrite_from_dataframe(new_toml_dataframe, original_toml_path)
   invisible(original_toml_path)
+}
+
+
+check_toml_dataframe <- function(toml_dataframe) {
+  expected_columns <-
+    c("type", "name", "order", "skip_if_fail", "created")
+
+  non_valid_names <-
+    !names(toml_dataframe) %in% expected_columns
+
+  if (any(non_valid_names)) {
+    bad_names <- names(toml_dataframe)[which(non_valid_names)]
+    log_message(
+      paste(
+        "The following columns are not valid and will be dropped:",
+        paste(bad_names, collapse = ", ")
+      )
+    )
+  }
+
+  toml_dataframe <-
+    toml_dataframe |>
+    dplyr::select(dplyr::any_of(expected_columns))
+
+  checkmate::assert_character(toml_dataframe$type)
+  checkmate::assert_character(toml_dataframe$name)
+  checkmate::assert_integerish(toml_dataframe$order)
+
+  if ("skip_if_fail" %in% names(toml_dataframe)) {
+    checkmate::assert_logical(toml_dataframe$skip_if_fail)
+  }
+
+  checkmate::assert_date(toml_dataframe$created)
+
+  toml_dataframe
 }
