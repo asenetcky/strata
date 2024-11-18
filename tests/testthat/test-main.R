dc999d9134c7ae2e64df51863bce3ec79dfde172
asenetcky <asenetcky@gmail.com>
2024-11-18 12:31:30 -0500

    reorg tests

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -2,50 +2,7 @@ test_that("main with no path", {
   expect_error(main())
 })
 
-test_that("main returns execution plan", {
-  tmp <- fs::dir_create(fs::file_temp())
-  strata::build_stratum(
-    path = tmp,
-    stratum_name = "first_stratum",
-    order = 1
-  )
-  stratum_path <-
-    fs::path(
-      tmp, "strata", "first_stratum"
-    )
-  strata::build_lamina(
-    stratum_path = stratum_path,
-    lamina_name = "first_lamina",
-    order = 1
-  )
-  strata::build_lamina(
-    stratum_path = stratum_path,
-    lamina_name = "second_lamina",
-    order = 2
-  )
 
-  first_lamina_code <- fs::path(stratum_path, "first_lamina", "my_code1.R")
-  second_lamina_code <- fs::path(stratum_path, "second_lamina", "my_code2.R")
-
-  my_code1 <- fs::file_create(first_lamina_code)
-  my_code2 <- fs::file_create(second_lamina_code)
-  cat(file = my_code1, "print('Hello, World!')")
-  cat(file = my_code2, "print('Goodbye, World!')")
-
-  execution_plan <- main(tmp)
-  expect_equal(
-    execution_plan |> dplyr::mutate(path = as.character(path)),
-    tibble::tibble(
-      stratum = c("first_stratum", "first_stratum"),
-      lamina = c("first_lamina", "second_lamina"),
-      order = c(1,2),
-      skip_if_fail = c(FALSE, FALSE),
-      created = lubridate::today(),
-      script = c("my_code1", "my_code2"),
-      path = c(first_lamina_code, second_lamina_code)
-    )
-  )
-})
 
 test_that("main is silent", {
   tmp <- fs::dir_create(fs::file_temp())
@@ -78,4 +35,41 @@ test_that("main is silent", {
   cat(file = my_code2, "my_colors <- c('Red', 'Blue', 'Green')")
 
   expect_silent(main(tmp, silent = TRUE))
+})
+
+
+test_that("main returns ane xecution plan", {
+  tmp <- fs::dir_create(fs::file_temp())
+  strata::build_stratum(
+    path = tmp,
+    stratum_name = "first_stratum",
+    order = 1
+  )
+  stratum_path <-
+    fs::path(
+      tmp, "strata", "first_stratum"
+    )
+  strata::build_lamina(
+    stratum_path = stratum_path,
+    lamina_name = "first_lamina",
+    order = 1
+  )
+  strata::build_lamina(
+    stratum_path = stratum_path,
+    lamina_name = "second_lamina",
+    order = 2
+  )
+
+  first_lamina_code <- fs::path(stratum_path, "first_lamina", "my_code1.R")
+  second_lamina_code <- fs::path(stratum_path, "second_lamina", "my_code2.R")
+
+  my_code1 <- fs::file_create(first_lamina_code)
+  my_code2 <- fs::file_create(second_lamina_code)
+  cat(file = my_code1, "print('Hello, World!')")
+  cat(file = my_code2, "print('Goodbye, World!')")
+
+  execution_plan <- main(tmp)
+
+  expect_true(checkmate::check_data_frame(execution_plan))
+
 })
