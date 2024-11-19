f0369ce84655b2563f1e6751e8c7aa4486d763c7
asenetcky <asenetcky@gmail.com>
2024-11-19 15:29:01 -0500

    add section on .tomls

--- R/main.R
+++ R/main.R
@@ -10,6 +10,24 @@
 #' and should be the target for automation. However, `main()` can be called
 #' from anywhere, and users can opt to not use `main.R` at all.
 #'
+#' @section .toml files:
+#'
+#' There are two types of `.toml` files that `main()` will read:
+#' * `.strata.toml` - a singular file inside the `<project_path>/strata` folder
+#' * `.laminae.toml` - a file inside each `<project_path>/strata/<stratum_name>`
+#' folder
+#'
+#' These files are created by the `strata` functions and are used to determine
+#' primarily the order of execution for the strata and laminae.  Anything not
+#' referenced by a .toml will be ignored by `main()` and other functions such as
+#' [survey_strata()], [adhoc_stratum], and [adhoc_lamina]. Users can safely add
+#' other folders and files in the project root, and even within the subfolders
+#' and they will be ignored, unless users have code known by a `.toml` that
+#' references them.
+#'
+#' Users can use the functions [survey_tomls] and [view_toml] to find and view
+#' the `.toml` files in their project.
+#'
 #' @param project_path A path to strata project folder.
 #' @param silent Suppress log messages? If `FALSE` (the default), log messages
 #' will be printed to the console. If `TRUE`, log messages will be suppressed.
