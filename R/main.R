6994e1174a634074818532b8e6569b110857c87d
asenetcky <asenetcky@gmail.com>
2024-11-19 13:46:39 -0500

    update main() doco

--- R/main.R
+++ R/main.R
@@ -1,4 +1,14 @@
-#' Entry point and automation target for your strata project
+#' Execute entire strata project
+#'
+#' @description
+#' `main()` will read the `.toml` files inside the `project_path` and begin
+#' sourcing the `strata` and `laminae` in the order specified by the user,
+#' with or without logging messages.
+#'
+#' When a strata project is created `main.R` is added to the project root.
+#' This script houses `main()`, and this file is the entry point to the project
+#' and should be the target for automation. However, `main()` can be called
+#' from anywhere, and users can opt to not use `main.R` at all.
 #'
 #' @param project_path A path to strata project folder
 #' @param silent Suppress log messages? If `FALSE` (the default), log messages
@@ -8,9 +18,10 @@
 #' @export
 #'
 #' @examples
-#' \dontrun{
-#' main("/PATH/TO/PROJECT/FOLDER")
-#' }
+#' tmp <- fs::dir_create(fs::file_temp())
+#' result <- strata::build_quick_strata_project(tmp, 1, 1)
+#' main(tmp)
+#' fs::dir_delete(tmp)
 main <- function(project_path, silent = FALSE) {
   project_path <- fs::path(project_path)
 
