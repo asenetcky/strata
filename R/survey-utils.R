2f0ead2e96566fd204b4128abfc9e9a733541ebb
asenetcky <asenetcky@users.noreply.github.com>
2024-12-12 22:30:16 +0000

    Style code (GHA)

--- R/survey-utils.R
+++ R/survey-utils.R
@@ -15,7 +15,7 @@ scout_path <- function(path) {
   bad_paths <- path[!is_dir & !is_file]
 
   # if not dir or file abort
-  if (length(bad_paths) > 0 ) {
+  if (length(bad_paths) > 0) {
     msg <-
       glue::glue(
         "Path must be an accessible directory or a file:
