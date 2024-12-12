b5b532181d03e8f833ed8bd70f1d57becbb2bb08
asenetcky <asenetcky@gmail.com>
2024-12-12 10:59:00 -0500

    add wip scout_path code

--- R/survey-utils.R
+++ R/survey-utils.R
@@ -2,8 +2,17 @@ scout_path <- function(path) {
   # check user input
   checkmate::assert_character(path)
 
-  project_path <- fs::path(project_path)
+  path <- fs::path(path)
 
-  # check if project_path exists
-  checkmate::assert_true(fs::dir_exists(project_path))
+  if (fs::is_dir(path)) {
+    # check if path exists
+    checkmate::assert_true(fs::dir_exists(path))
+  }
+
+ if (fs::is_file(path)) {
+    # check if path exists
+    checkmate::assert_true(fs::file_exists(path))
+ }
+
+  invisible(path)
 }
