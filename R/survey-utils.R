98cb73923c1b6b6db77a35e3348b9308b72ec538
asenetcky <asenetcky@gmail.com>
2024-12-12 18:16:03 -0500

    add early scout_project func

--- R/survey-utils.R
+++ R/survey-utils.R
@@ -15,12 +15,12 @@ scout_path <- function(path) {
   bad_paths <- path[!is_dir & !is_file]
 
   # if not dir or file abort
-  if (length(bad_paths) > 0) {
+  if (length(bad_paths) > 0 ) {
     msg <-
       glue::glue(
         "Path must be an accessible directory or a file:
         ",
-        paste(bad_paths, collapse = ", ")
+        paste(paste0("'",bad_paths, "'"), collapse = ", ")
       )
     rlang::abort(msg)
   }
@@ -29,5 +29,41 @@ scout_path <- function(path) {
 }
 
 
+scout_project <- function(path) {
+  # assumptions
+  has_strata <- FALSE
+  has_laminae <- FALSE
 
-# check if strataproject
+  # check path input
+  path <- scout_path(path)
+
+  # check if path is a strata project
+  strata_folder <- fs::path(path, "strata")
+  strata_exist <- fs::dir_exists(strata_folder)
+
+  # check if tomls exists
+  tomls <- find_tomls(path)
+
+  if (!purrr::is_empty(tomls)) {
+    has_strata_toml <-
+      tomls |>
+      fs::path_file() |>
+      stringr::str_detect(stringr::fixed(".strata.toml")) |>
+      any()
+
+    has_laminae <-
+      tomls |>
+      fs::path_file() |>
+      stringr::str_detect(stringr::fixed(".laminae.toml")) |>
+      any()
+
+    has_strata <- all(has_strata_toml, has_laminae_toml)
+  }
+
+  if (!any(has_strata, has_laminae)) {
+    msg <- glue::glue("'{path}' is not a strata project")
+    rlang::abort(msg)
+  }
+
+  invisible(path)
+}
