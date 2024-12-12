34419faae02eba855be7eca75b50c8518d4b053a
asenetcky <asenetcky@gmail.com>
2024-12-12 13:07:34 -0500

    add clarifying comments

--- R/survey-utils.R
+++ R/survey-utils.R
@@ -6,13 +6,14 @@ scout_path <- function(path) {
 
   # check path type
   is_dir <- fs::is_dir(path)
-
   is_file <- FALSE
 
+  # check if file
   if (!is_dir) {
     is_file <- fs::is_file(path)
   }
 
+  # if not dir or file abort
   if (!is_dir && !is_file) {
     rlang::abort("Path must be an existing, accessible directory or a file")
   }
