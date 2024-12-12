d2c9f53caac0bf03e800824a2073ce254b610842
asenetcky <asenetcky@gmail.com>
2024-12-12 13:45:14 -0500

    attempt to make vector friendly

--- R/survey-utils.R
+++ R/survey-utils.R
@@ -6,16 +6,23 @@ scout_path <- function(path) {
 
   # check path type
   is_dir <- fs::is_dir(path)
-  is_file <- FALSE
+  is_file <- rep(FALSE, length(path))
 
   # check if file
-  if (!is_dir) {
+  if (!any(is_dir)) {
     is_file <- fs::is_file(path)
   }
+  bad_paths <- NULL
+  bad_paths <- path[!is_dir & !is_file]
 
   # if not dir or file abort
-  if (!is_dir && !is_file) {
-    rlang::abort("Path must be an existing, accessible directory or a file")
+  if (!purrr::is_empty(bad_paths)) {
+    msg <-
+      paste(
+        "Path must be an existing, accessible directory or a file",
+        paste(bad_paths, collapse = ", ")
+            )
+    rlang::abort(msg)
   }
 
   invisible(path)
