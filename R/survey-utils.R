2858bafba96172f65b39fe047ae3004c5075b72f
asenetcky <asenetcky@gmail.com>
2024-12-12 12:46:57 -0500

    add early scout_path to survey-utils

--- R/survey-utils.R
+++ R/survey-utils.R
@@ -4,15 +4,19 @@ scout_path <- function(path) {
 
   path <- fs::path(path)
 
-  if (fs::is_dir(path)) {
-    # check if path exists
-    checkmate::assert_true(fs::dir_exists(path))
+  # check path type
+  is_dir <- fs::is_dir(path)
+
+  is_file <- FALSE
+
+  if (!is_dir) {
+    is_file <- fs::is_file(path)
   }
 
-  if (fs::is_file(path)) {
-    # check if path exists
-    checkmate::assert_true(fs::file_exists(path))
+  if (!is_dir && !is_file) {
+    rlang::abort("Path must be an existing, accessible directory or a file")
   }
 
   invisible(path)
 }
+
