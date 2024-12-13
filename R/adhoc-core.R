9ebe2022596ac06165a86a223599c02e3d622d37
asenetcky <asenetcky@users.noreply.github.com>
2024-12-13 15:02:49 +0000

    Style code (GHA)

--- R/adhoc-core.R
+++ R/adhoc-core.R
@@ -30,18 +30,18 @@ adhoc_stratum <- function(stratum_path, silent = FALSE) {
   stratum_path <- scout_path(stratum_path)
   stratum_name <- fs::path_file(stratum_path)
 
-    project_path <-
-      fs::path_dir(
-        fs::path_dir(stratum_path)
-      )
+  project_path <-
+    fs::path_dir(
+      fs::path_dir(stratum_path)
+    )
 
-    execution_plan <-
-      build_execution_plan(project_path) |>
-      dplyr::filter(.data$stratum == stratum_name)
+  execution_plan <-
+    build_execution_plan(project_path) |>
+    dplyr::filter(.data$stratum == stratum_name)
 
-    run_execution_plan(execution_plan, silent)
+  run_execution_plan(execution_plan, silent)
 
-    invisible(execution_plan)
+  invisible(execution_plan)
 }
 
 #' Execute a single lamina ad hoc
@@ -101,23 +101,22 @@ adhoc <- function(name, prompt = TRUE, project_path = NULL) {
   # interactive only
   if (!interactive()) {
     rlang::abort("This function is for interactive only")
-
   }
   # check user input
   checkmate::assert_character(name)
   checkmate::assert_logical(prompt)
 
-    # if no path use working directory
-    if (is.null(project_path)) {
-      project_path <- fs::path_wd()
-    }
+  # if no path use working directory
+  if (is.null(project_path)) {
+    project_path <- fs::path_wd()
+  }
 
-    # check for project path existence
-    if (!fs::dir_exists(project_path)) stop("Cannot find strata project")
+  # check for project path existence
+  if (!fs::dir_exists(project_path)) stop("Cannot find strata project")
 
-    survey <- survey_strata(project_path)
+  survey <- survey_strata(project_path)
 
-    # placeholder execution plan
-    execution_plan <- NULL
-    invisible(execution_plan)
+  # placeholder execution plan
+  execution_plan <- NULL
+  invisible(execution_plan)
 }
